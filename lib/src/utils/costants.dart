c09e29bc51723c5e7a937fc28caef48cc4ff5cac
abrar-5888 <abrararshad32@gmail.com>
2025-10-08 16:10:21 +0500

    changed url for testing

--- lib/src/utils/costants.dart
+++ lib/src/utils/costants.dart
@@ -1,3 +1,14 @@
+// class BapgConstants {
+//   BapgConstants._();
+//
+//   static const String bapgBaseUrl = 'https://payments.bankalfalah.com';
+//   static const String bapgEndpoint = '/HS/HS/HS';
+//
+//   // Response codes Failure/Success
+//   static const String successCode = 'RC=00';
+//   static const String failureCode = 'RC=01';
+// }
+
 class BapgConstants {
   BapgConstants._();
 
